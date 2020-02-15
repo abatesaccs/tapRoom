bcd78171a314e47cd37de02cbac28cf2327bea00
Andrew Bates <Abatesaccs@gmail.com>
2020-02-14 16:39:11 -0800

    add form to addkeg page

--- taproom/src/components/AddKeg.jsx
+++ taproom/src/components/AddKeg.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
-import IIHO from '../assets/images/ifIHadOne.jpg';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+
+const cardPosition = {
+  marginLeft: '35hv',
+  textAlign: 'center'
+};
+
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: '5vw',
+    width: '40vw',
+    maxWidth: '500px',
+    marginTop: '5vh',
+    height: '490px',
+    paddingBottom: '5vh',
+    margin: 'auto'
+  },
+  button: {
+    margin: 'auto'
+  }
+});
 
 function AddKeg(){
+  const classes = useStyles();
   return(
-    <div>
-      <p>Here's where id put my form, </p>
-      <img src={IIHO} />
+    <div style={cardPosition}>
+      <Card className={classes.root} variant='outlined'>
+        <CardContent>
+          <form>
+            <h3>Add Keg</h3>
+            <TextField id="filled-basic" label="Brewery" variant="filled" />
+            <br/><br/>
+            <TextField id="filled-basic" label="Name" variant="filled" />
+            <br/><br/>
+            <TextField id="filled-basic" label="ABV" variant="filled" />
+            <br/><br/>
+            <TextField id="filled-basic" label="Category" variant="filled" />
+            <br/><br/>
+            <TextField id="filled-basic" label="Price" variant="filled" />
+          </form>
+        </CardContent>
+        <CardActions>
+          <Button className={classes.button} variant='contained' size='medium' color='#33c9dc' to='/menu' component={Link}>Enter</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
