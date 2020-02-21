ab77a0d6965e8fef71b2e03e555c2037d2a35116
Andrew Bates <ABatesAccs@gmail.com>
2020-02-21 15:51:57 -0800

    use state hooks to get material ui form working

--- taproom/src/components/AddKeg.jsx
+++ taproom/src/components/AddKeg.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
-import { getThemeProps } from '@material-ui/styles';
 
 const cardPosition = {
   marginLeft: '35hv',
@@ -32,42 +29,38 @@ const useStyles = makeStyles({
 
 function AddKeg(props){
   const classes = useStyles();
-  let _brewery = null; 
-  let _name = null;
-  let _abv = null;
-  let _category = null;
-  let _price = null;
+
+  const [brewery, setBrewery] = useState('');
+  const [name, setName] = useState('');
+  const [abv, setAbv] = useState('');
+  const [category, setCategory] = useState('');
+  const [price, setPrice] = useState('');
 
   function handleNewKegSubmission(event) {
     event.preventDefault();
-    props.onNewKegCreation({brewery: _brewery.value, name: _name.value, abv: _abv.value, category: _category.value, price: _price.value});
-    _brewery.value = ''; 
-    _name.value = '';
-    _abv.value = '';
-    _category.value = '';
-    _price.value = '';
+    props.onNewKegCreation({brewery: brewery, name: name, abv: abv, category: category, price: price});
   }
 
   return(
     <div style={cardPosition}>
       <Card className={classes.root} variant='outlined'>
         <CardContent>
-          {/* <form onSubmit={handleNewKegSubmission}>
-            <h3>Add Keg</h3>
-            <TextField id="filled-basic" type='text' label="Brewery" variant="filled" ref={(TextField) => {_brewery = TextField;}} />
-            <br/><br/>
-            <TextField id="filled-basic" type='text' label="Name" variant="filled" ref={(TextField) => {_name = TextField;}} />
-            <br/><br/>
-            <TextField id="filled-basic" type='text' label="ABV" variant="filled" ref={(TextField) => {_abv = TextField;}} />
-            <br/><br/>
-            <TextField id="filled-basic" label="Category" variant="filled" type='text' ref={(TextField) => {_category = TextField;}} />
-            <br/><br/>
-            <TextField id="filled-basic" type='text' label="Price" variant="filled" ref={(TextField) => {_price = TextField;}} />
-            <br/><br/>
-            <Button className={classes.button} variant='contained' size='medium' color='secondary' type='submit'>Enter</Button>
-          </form> */}
-
           <form onSubmit={handleNewKegSubmission}>
+            <h3>Add Keg</h3>
+            <TextField id="filled-basic" type='text' label="Brewery" variant="filled" value={brewery} onInput={ e=>setBrewery(e.target.value)} />
+            <br/><br/>
+            <TextField id="filled-basic" type='text' label="Name" variant="filled" value={name} onInput={ e=>setName(e.target.value)} />
+            <br/><br/>
+            <TextField id="filled-basic" type='text' label="ABV" variant="filled" value={abv} onInput={ e=>setAbv(e.target.value)} />
+            <br/><br/>
+            <TextField id="filled-basic" label="Category" variant="filled" type='text' value={category} onInput={ e=>setCategory(e.target.value)} />
+            <br/><br/>
+            <TextField id="filled-basic" type='text' label="Price" variant="filled" value={price} onInput={ e=>setPrice(e.target.value)} />
+            <br/><br/>
+            <Button className={classes.button} variant='contained' size='medium' color='primary' type='submit'>Enter</Button>
+          </form>
+
+          {/* <form onSubmit={handleNewKegSubmission}>
             <input
               type='text'
               id='brewery'
@@ -94,12 +87,9 @@ function AddKeg(props){
               placeholder='Price'
               ref={(input) => {_price = input;}}/>
             <button type='submit'>Help!</button>
-          </form>
+          </form> */}
 
         </CardContent>
-        {/* <CardActions> */}
-        {/* <Button className={classes.button} variant='contained' size='medium' color='#33c9dc' type='submit'>Enter</Button> */}
-        {/* </CardActions> */}
       </Card>
     </div>
   );
