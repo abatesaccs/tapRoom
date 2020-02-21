3533f55367dc46149f6c0e5fcdb553e2a9fd60cf
Andrew Bates <ABatesAccs@gmail.com>
2020-02-21 11:38:51 -0800

    setup app to use state

--- taproom/src/components/App.jsx
+++ taproom/src/components/App.jsx
@@ -8,32 +8,44 @@ import MenuList from './MenuList';
 import AddKeg from './AddKeg';
 import Cowbell from './Cowbell';
 
-function App(){
-  var style = {  
-    backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(' + Background + ')',
-    backgroundPosition: 'fixed',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'repeat',
-    minHeight: '100vh',
-    minWidth: '100%',
-    color: 'white',
-    fontFamily: 'IBM Plex Sans, sans-serif',
-    fontWeight: '200'
-  };
-  return (
-    <div style={style}>
-      <div>
-        <Header/>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route path='/menu' component={MenuList} />
-          <Route path='/addKeg' component={AddKeg}/>
-          <Route path='/cowbell' component={Cowbell}/> {/* easter egg */} 
-          <Route component={Error404} />
-        </Switch>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKegList: []
+
+    };
+    this.hanleAddingNewKeg = this.hanleAddingNewKeg.bind(this);
+  }
+
+  render(){
+    var style = {  
+      backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(' + Background + ')',
+      backgroundPosition: 'fixed',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'repeat',
+      minHeight: '100vh',
+      minWidth: '100%',
+      color: 'white',
+      fontFamily: 'IBM Plex Sans, sans-serif',
+      fontWeight: '200'
+    };
+    return (
+      <div style={style}>
+        <div>
+          <Header/>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route path='/menu' component={MenuList} />
+            <Route path='/addKeg' component={AddKeg}/>
+            <Route path='/cowbell' component={Cowbell}/> {/* easter egg */} 
+            <Route component={Error404} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
  
 export default App;
